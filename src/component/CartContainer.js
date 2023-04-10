e13a71f4a1e479e07cfe818b584eb71b63a01418
Kagheni <Kaghenimbale@gmail.com>
2023-04-10 16:10:07 +0200

    Add decrease and increase function

--- src/component/CartContainer.js
+++ src/component/CartContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart } from '../features/cart/CartSlice';
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((state) => state.cart);
 
   if (amount < 1) {
@@ -32,7 +34,9 @@ const CartContainer = () => {
             Total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
