ea4072330c72858161e5b432c5c1d47b0807106b
Kagheni <Kaghenimbale@gmail.com>
2023-04-10 14:44:12 +0200

    Add Ui of the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CartContainer from './component/CartContainer';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
